7915531c97683314a0279d09e8f46577467e5e6c
voidcode100 <spunckcore@gamil.com>
2025-07-25 20:02:24 +0800

    拆分微服务，FeignClient发送请求

---
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -0,0 +1,21 @@
+package com.hmall.api.client;
+
+
+import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Set;
+
+@FeignClient("item-service")
+public interface ItemClient {
+    @GetMapping("/items")
+    List<ItemDTO> queryItemByIds(@RequestParam("ids") Set<Long> ids);
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+}
